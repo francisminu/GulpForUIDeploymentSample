5a5aa6017e21aa84ed666eab635e86d21934cbd1
mfrancis <minu.francis@geodis.com>
2018-07-05 11:06:30 -0500

    Feat: added colors to the messages

--- deployment/gulpfile.js
+++ deployment/gulpfile.js
@@ -11,28 +11,43 @@ var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 var appconfig = require('./appconfig.json');
 var neoAsync = require('neo-async');
+var colors = require('colors');
 
 let args = {};
 let baseDirectory = '../';
 
+var infoLog = colors.green;
+var debugLog = colors.green;
+var warningLog = colors.green;
+var errorLog = colors.green;
+var userInput = colors.cyan;
+
+// colors.setTheme({
+//     info: 'green',
+//     warn: 'yellow',
+//     debug: 'blue',
+//     error: 'red',
+//     userInput: 'italic'
+// });
+
 
 gulp.task('prepare-qa', (done) => {
     runSequence(
-        'qa-select-branch',
-        'git-clean',
-        'git-checkout',
-        'git-pull',
-        'get-semantic-version',
-        'git-add',
-        'git-commit',
-        'git-tag',
-        'git-push',
-        'git-push-tags',
-        'build-files',
-        'zip-files',
+        // 'qa-select-branch',
+        // 'git-clean',
+        // 'git-checkout',
+        // 'git-pull',
+        // 'get-semantic-version',
+        // 'git-add',
+        // 'git-commit',
+        // 'git-tag',
+        // 'git-push',
+        // 'git-push-tags',
+        // 'build-files',
+        // 'zip-files',
         'get-credentials',
-        'get-qa-servers',
-        'login-and-copy-to-servers',
+        // 'get-qa-servers',
+        // 'login-and-copy-to-servers',
         done);
 });
 
@@ -41,9 +56,9 @@ gulp.task('qa-select-branch', (done) => {
         .pipe(prompt.prompt({
             type: 'input',
             name: 'branchName',
-            message: 'Enter the git branch to deploy from [development]'
+            message: userInput('Enter the git branch to deploy from [development]')
         }, function (res) {
-            console.log('Chosen branch is: ', res.branchName);
+            console.log(infoLog('Chosen branch is: ', res.branchName));
             args.branchName = res.branchName;
             done();
         }));
@@ -51,33 +66,42 @@ gulp.task('qa-select-branch', (done) => {
 
 gulp.task('git-clean', (done) => {
     gulpGit.clean({ args: '-f' }, (err) => {
-        if (err) return done(err);
-        console.log('Cleaned the branch successfully');
+        if (err) {
+            console.log(errorLog('git-clean failed ', err));
+            return done(err);
+        }
+        console.log(infoLog('Cleaned the branch successfully'));
         done();
     });
 });
 
 gulp.task('git-checkout', (done) => {
     gulpGit.checkout(args.branchName, (err) => {
-        if (err) return done(err);
-        console.log('Branch ', args.branchName + ' Checked Out successfully');
+        if (err) {
+            console.log(errorLog('git-checkout failed.'));
+            return done(err);
+        }
+        console.log(infoLog('Branch ', args.branchName + ' Checked Out successfully'));
         done();
     });
 });
 
 gulp.task('git-pull', (done) => {
     gulpGit.pull('origin', args.branchName, { args: '--rebase' }, (err) => {
-        if (err) return done(err);
-        console.log('Branch ', args.branchName + ' pulled successfully from Origin');
+        if (err) {
+            console.log(errorLog('git-pull failed'));
+            return done(err);
+        }
+        console.log(infoLog('Branch ', args.branchName + ' pulled successfully from Origin'));
         done();
     });
 });
 
 gulp.task('get-semantic-version', (done) => {
     var currentVersion = packageJson.version;
-    console.log('Current Version: ', currentVersion);
+    console.log(infoLog('Current Version: ', currentVersion));
     var newVersion = semver.inc(currentVersion, 'prerelease', 'qa');
-    console.log('New Version: ', newVersion);
+    console.log(infoLog('New Version: ', newVersion));
     args.newVersion = newVersion;
     gulp.src([baseDirectory + 'package.json'])
         .pipe(gulpBump({
@@ -88,84 +112,69 @@ gulp.task('get-semantic-version', (done) => {
     done();
 });
 
-// gulp.task('gulp-bump', (done) => {
-//     console.log('About to bump the version.');
-//     gulp.src('./package.json')
-//         .pipe(prompt.prompt({
-//             type: 'input',
-//             name: 'releaseType',
-//             message: 'Is it a major/minor/patch release?'
-//         }, (res) => {
-//             args.releaseType = res.releaseType;
-//             gulp.src('./package.json')
-//                 .pipe(gulpBump({ type: args.releaseType }))
-//                 .pipe(gulp.dest('./'));
-//             done();
-//         }));
-// });
-
 gulp.task('git-add', (done) => {
-    console.log('Staging package.json');
+    console.log(infoLog('Staging package.json'));
     return gulp.src([baseDirectory + 'package.json'])
         .pipe(gulpGit.add());
 });
 
 gulp.task('git-commit', (done) => {
-    console.log('Commiting package.json');
+    console.log(infoLog('Commiting package.json'));
     return gulp.src([baseDirectory + 'package.json'])
         .pipe(gulpGit.commit('build(release): version upgraded to v' + args.newVersion));
 });
 
 gulp.task('git-tag', (done) => {
     gulpGit.tag('v' + args.newVersion, 'Tag created: ' + args.newVersion, (err) => {
-        if (err) return done(err);
-        console.log('Tagged the ' + args.branchName + ' branch with Version ' + args.newVersion);
+        if (err) {
+            console.log(errorLog('git-tag failed'));
+            return done(err);
+        }
+        console.log(infoLog('Tagged the ' + args.branchName + ' branch with Version ' + args.newVersion));
         done();
     });
 });
 
 gulp.task('git-push', (done) => {
-    console.log('Push commits to origin');
+    console.log(infoLog('Push commits to origin'));
     gulpGit.push('origin', args.branchName, (err) => {
-        if (err) return done(err);
-        console.log('Pushed ' + args.branchName + ' to origin/' + args.branchName);
+        if (err) {
+            console.log(errorLog('git-push failed'));
+            return done(err);
+        }
+        console.log(infoLog('Pushed ' + args.branchName + ' to origin/' + args.branchName));
         done();
     });
 });
 
 gulp.task('git-push-tags', (done) => {
-    console.log('Pushing all the tags from local to remote');
+    console.log(infoLog('Pushing all the tags from local to remote'));
     gulpGit.exec({ args: 'push --tags' }, (err) => {
-        if (err) return done(err);
-        console.log('All tags pushed from local to remote');
+        if (err) {
+            console.log(errorLog('git-push-tags failed'));
+            return done(err);
+        }
+        console.log(infoLog('All tags pushed from local to remote'));
         done();
     });
 });
 
 gulp.task('build-files', (done) => {
     exec('ng build --prod', (err, stdout, stderr) => {
-        if (err) return done(err);
+        if (err) {
+            console.log(errorLog('build-files failed. Error: ', err));
+            return done(err);
+        }
         done();
     });
 });
 
-// gulp.task('tar-and-zip', (done) => {
-//     console.log('Zipping the target files');
-//     exec('"./utils/7zip/7za.exe" a -ttar -so archive.tar rfframework | "../utils/7zip/7za.exe" a -si "output/rfframework.tgz"',
-//     (err, stdout, stderr) => {
-//       console.log(stdout);
-//       console.log(stderr);
-//       callback(err);
-//     });
-//   console.log(successFormat('Compressed the src files successfully!!!'));
-// });
-
 gulp.task('zip-files', (done) => {
-    console.log('Zipping files..');
+    console.log(infoLog('Zipping files..'));
     gulp.src(baseDirectory + 'dist/*')
         .pipe(zip('buildFiles.zip'))
         .pipe(gulp.dest('./output'));
-    console.log('Zipping complete.');
+    console.log(infoLog('Zipping complete.'));
     done();
 });
 
@@ -175,7 +184,7 @@ gulp.task('get-credentials', (done) => {
             [{
                 type: 'input',
                 name: 'username',
-                message: 'Please enter the username: ',
+                message: userInput('Please enter the username: '),
                 validate: (username) => {
                     if (username === '') return false;
                     else return true;
@@ -184,7 +193,7 @@ gulp.task('get-credentials', (done) => {
             {
                 type: 'password',
                 name: 'password',
-                message: 'Please enter the password: ',
+                message: userInput('Please enter the password: '),
                 validate: (password) => {
                     if (password === '') return false;
                     else return true;
@@ -193,12 +202,13 @@ gulp.task('get-credentials', (done) => {
             ], (res) => {
                 args.username = 'ohl\\' + res.username;
                 args.password = res.password;
-                console.log('Username and Password obtained.');
+                console.log(infoLog('Username and Password obtained.'));
                 done();
             }));
 });
 
 gulp.task('get-qa-servers', (done) => {
+    console.log(infoLog('Fetching QA Servers...'));
     let serverList = [];
     for (let index = 0; index < appconfig.qa_servers.length; index++) {
         serverList.push(appconfig.qa_servers[index].ip + '(' + appconfig.qa_servers[index].name + ')');
@@ -207,7 +217,7 @@ gulp.task('get-qa-servers', (done) => {
         .pipe(prompt.prompt({
             type: 'checkbox',
             name: 'servers',
-            message: 'Choose the QA servers for deployment: ',
+            message: userInput('Choose the QA servers for deployment: '),
             choices: serverList,
             validate: (servers) => {
                 let serverList = servers || [];
@@ -216,7 +226,7 @@ gulp.task('get-qa-servers', (done) => {
             }
         }, function (res) {
             args.servers = res.servers;
-            console.log('Servers are: ', args.servers);
+            console.log(infoLog('QA Servers are: ', args.servers));
             done();
         }));
 });
@@ -234,17 +244,19 @@ gulp.task('login-and-copy-to-servers', (done) => {
             exclude: [],
             deleteRoot: true
         };
-        console.log('Copying files to ' + server + ' started');
+        console.log(infoLog('Copying files to ' + server + ' started'));
         ftpDeploy.deploy(ftpConfig, (err) => {
-            if (err) return done(err)
+            if (err) {
+                console.log(errorLog('login-and-copy-to-servers failed. Error: ', err));
+                return done(err);
+            }
             done();
         });
         ftpDeploy.on('uploaded', function (data) {
-            console.log('On Upload');
-            console.log(data);
+            console.log(infoLog('Uploaded the data'));
         });
     }, (result) => {
-        console.log('Copied files to all the servers!!!');
+        console.log(infoLog('Copied files to all the servers!!!'));
         done();
     });
 });
