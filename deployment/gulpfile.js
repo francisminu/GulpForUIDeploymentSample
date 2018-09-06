17a4531ba9debab27feab1685930ef46979db6c5
Minu Francis <mfrancis@5CG651017G-LA>
2018-09-06 15:10:40 -0500

    Changes for prepare-qa added

--- deployment/gulpfile.js
+++ deployment/gulpfile.js
@@ -26,7 +26,7 @@ var userInput = colors.cyan;
 
 gulp.task('prepare-qa', (done) => {
     runSequence(
-        'qa-select-branch',
+        'get-qa-branch',
         'git-clean',
         'git-checkout',
         'git-pull',
@@ -44,7 +44,28 @@ gulp.task('prepare-qa', (done) => {
         done);
 });
 
-gulp.task('qa-select-branch', (done) => {
+gulp.task('prepare-uat', (done) => {
+    runSequence(
+        'get-all-release-branches',
+        'get-release-branch',
+        'git-clean',
+        'git-checkout',
+        'git-pull',
+        'get-semantic-version',
+        'git-add',
+        'git-commit',
+        'git-tag',
+        'git-push',
+        'git-push-tags',
+        // 'build-files',
+        // 'zip-files',
+        // 'get-credentials',
+        // 'get-qa-servers',
+        // 'login-and-copy-to-servers',
+        done);
+});
+
+gulp.task('get-qa-branch', (done) => {
     args.currentDeploymentEnv = 'qa';
     gulp.src(baseDirectory + 'package.json')
         .pipe(prompt.prompt({
@@ -58,13 +79,92 @@ gulp.task('qa-select-branch', (done) => {
         }));
 });
 
+gulp.task('get-all-release-branches', (done) => {
+    gulpGit.exec({ args: 'branch -r', cwd: baseDirectory }, (err, res) => {
+        if (err) {
+            console.log(errorLog('get-all-release-branches failed. Exiting deployment...'))
+            throw (err);
+        }
+        res = res.replace('\'', '');
+        // console.log(infoLog('Remote branches are: ', res));
+        let remoteBranches = res.split('\n');
+        args.releaseBranches = [];
+        for (let index = 0; index < remoteBranches.length; index++) {
+            let branch = remoteBranches[index].replace(/\s/g, '');
+            if (branch.startsWith('origin/release/')) {
+                args.releaseBranches.push(branch.split('origin/')[1]);
+            }
+        }
+        console.log(infoLog('Release branches are: ', args.releaseBranches));
+        done();
+    });
+});
+
+gulp.task('get-release-branch', (done) => {
+    if (args.releaseBranches.length === 1) {
+        args.branchName = args.releaseBranches[0];
+        console.log(infoLog('Release branch is: ', args.branchName));
+        done();
+    } else if (args.releaseBranches.length > 1) {
+        console.log(errorLog('More than one Release branch found. Exiting deployment...'));
+        done();
+    } else if (args.releaseBranches.length === 0) {
+        gulp.src(baseDirectory + 'package.json')
+            .pipe(prompt.prompt({
+                type: 'list',
+                name: 'releaseType',
+                message: userInput('Please select the release version type: '),
+                choices: ['major', 'minor']
+            }, function (res) {
+                args.releaseType = res.releaseType;
+                if (res.releaseType === 'major') {
+                    var currentVersion = packageJson.version;
+                    var newVersion = semver.inc(currentVersion, args.releaseType);
+                    console.log(infoLog('New version: ', newVersion));
+                    args.releaseBranchName = 'v' + (newVersion.split('')[0]);
+                    console.log(infoLog('Release branch name: ', args.releaseBranchName));
+                } else if (res.releaseType === 'minor') {
+                    var newVersion = packageJson.version.split('-')[0];
+                    console.log('new Version: ', newVersion);
+                    args.releaseBranchName = 'v' + (newVersion.split('-')[0]);
+                    console.log(infoLog('Release branch name: ', args.releaseBranchName));
+                }
+                CreateAndPublishReleaseBranch(() => {
+                    console.log(infoLog('CreateAndPublishReleaseBranch complete.'));
+                    done();
+                });
+            }));
+    }
+});
+
+let CreateAndPublishReleaseBranch = (done) => {
+    let gitflowCreateReleaseBranchCommand = 'flow release start ' + args.releaseBranchName;
+    gulpGit.exec({ args: gitflowCreateReleaseBranchCommand, cwd: baseDirectory }, (err, res) => {
+        if (err) {
+            console.log(errorLog('Creating RELEASE branch ', args.releaseBranchName, ' failed. Exiting deployment...'))
+            throw (err);
+        }
+        console.log(infoLog('Created Release branch ', args.releaseBranchName));
+        let gitflowPublishReleaseBranchCommand = 'flow release publish ' + args.releaseBranchName;
+        gulpGit.exec({ args: gitflowPublishReleaseBranchCommand, cwd: baseDirectory }, (err, res) => {
+            if (err) {
+                console.log(errorLog('Publishing RELEASE branch ', args.releaseBranchName, 'failed. Exiting deployment...'))
+                throw (err);
+            }
+            console.log(infoLog('Published Release branch ', args.releaseBranchName));
+            args.branchName = 'release/' + args.releaseBranchName;
+            done();
+        });
+    });
+}
+
 gulp.task('git-clean', (done) => {
     gulpGit.clean({ args: '-f' }, (err) => {
         if (err) {
             console.log(errorLog('git-clean failed ', err));
             return done(err);
         }
-        console.log(infoLog('Cleaned the branch successfully'));
+        console.log(infoLog('Cleaned the local repository successfully'));
         done();
     });
 });
@@ -154,7 +254,7 @@ gulp.task('git-push-tags', (done) => {
 });
 
 gulp.task('build-files', (done) => {
-    exec('ng build --environment='+args.currentDeploymentEnv, (err, stdout, stderr) => {
+    exec('ng build --environment=' + args.currentDeploymentEnv, (err, stdout, stderr) => {
         if (err) {
             console.log(errorLog('build-files failed. Error: ', err));
             return done(err);
